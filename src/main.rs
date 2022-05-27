4c2fd15d665f6649a55818f637d8b83f85da8c00
BLYKIM <vkfksql1@gmail.com>
2022-05-27 05:27:24 +0000

    ubt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("Hello, world!");
+    let con = String::from("Congraturations!! i'll be god");
+
+    println!("{}", con);
 }
--- target/debug/rust_win_test.d
+++ target/debug/rust_win_test.d
@@ -1,1 +1,1 @@
-D:\PROJECT\rust_dev\rust_win_test\target\debug\rust_win_test.exe: D:\PROJECT\rust_dev\rust_win_test\src\main.rs
+/home/bly/Desktop/rust_dev/win_rust/target/debug/rust_win_test: /home/bly/Desktop/rust_dev/win_rust/src/main.rs
